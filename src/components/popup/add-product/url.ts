486d9ebd925885403c90479e510d29d91a1e9435
Eduard-Kirilov <kev987.free@ya.ru>
2020-06-12 13:09:31 +0500

    cleanup

--- src/components/popup/add-product/url.ts
+++ src/components/popup/add-product/url.ts
@@ -3,8 +3,8 @@ import { IAllStringProps } from 'utils/interface';
 export const selectList: string[] = ['Macbook', 'Acer', 'Hp', 'Asus'];
 
 export const selectUrl: IAllStringProps = {
-  Macbook: '/api/v1/media/1-macbook.img?w=285&h=160',
-  Acer: '/api/v1/media/2-acer.img?w=285&h=160',
-  Hp: '/api/v1/media/3-hp.img?w=285&h=160',
-  Asus: '/api/v1/media/4-asus.img?w=285&h=160',
+  Macbook: '/api/v1/media/1-macbook.jpg?w=285&h=160',
+  Acer: '/api/v1/media/2-acer.jpg?w=285&h=160',
+  Hp: '/api/v1/media/3-hp.jpg?w=285&h=160',
+  Asus: '/api/v1/media/4-asus.jpg?w=285&h=160',
 };
